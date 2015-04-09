690b004699273b71ae7a44634702bfd752349454
Sergiy Dybskiy <sdybskiy@gmail.com>
2015-04-09 19:11:46 -0400

    added templates

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /article/5/
     url(r'^(?P<article_id>[0-9]+)/$', views.article, name='article'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the articles index.")
+from .models import Article
+
+
+class IndexView(generic.ListView):
+    template_name = 'articles/index.html'
+    context_object_name = 'article_list'
+
+    def get_queryset(self):
+        """Return all articles"""
+        return Article.objects.all()
+
 
 def article(request, article_id):
     return HttpResponse("You're looking at article %s." % article_id)
